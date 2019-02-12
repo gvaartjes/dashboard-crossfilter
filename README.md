fd3a9553e4f2dcafafca2ebdbe66828e9f646d39
gvaartjes <gert@highsoft.com>
2019-02-12 08:28:59 +0100

    Initial commit, working draft

---
+++ README.md
@@ -0,0 +1,6 @@
+# highcharts-cloud-charts-selector
+A dropdown selector for showing multiple charts one by one, served from Highcharts Cloud 
+
+# Installation
+1. `npm install`
+2. `npm start`
---
+++ js/index.js
@@ -0,0 +1,383 @@
+'use strict';
+/*global Highcharts*/
+
+const asyncFetch = async (url) => {
+  let response = await fetch(url)
+  return (response.ok) ? await response.json() : { msg: 'error fetching' }
+};
+
+let arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const gt = (nr) => {
+    return elem => elem > nr;
+  }
+
+  const lt = (nr) => {
+    return elem => elem < nr;
+  }
+
+  const between = (nr1, nr2) => {
+    return elem => elem > nr1 && elem < nr2;
+  }
+
+  const matches = (prop, val) => {
+    return x => x[prop] && x[prop] === val;
+  }
+
+  const and = (f1, f2) => {
+    return (x) => f1(x) && f2(x);
+  }
+
+  const or = (f1, f2) => (x) => f1(x) || f2(x);
+
+  // Array.map([0,1,2], (x) => x * 2)
+  // Array['map']([0,1,2], (x) => x * 2)
+  // const gt
+  // Array['filter']([0,1,2], gt2)
+
+
+  let gt4 = gt(4)
+  let between4and8 = between(4, 8)
+
+  let filters = [gt4, between4and8];
+
+  /**
+   * 
+   * @param {*} preds an Array of functions that filter the supplied array
+   * @param {*} arr 
+   */
+  const update = (preds, arr) => {
+    let res = null;
+    preds.map(p => {
+      (res == null) ? res = arr.filter(p) : res = res.filter(p);
+    });
+    return res;
+  }
+
+  let arr2 = update(filters, arr1)
+
+  /**
+   * 
+   * @param {Array} arr - Array of Objects
+   * @param {String} prop - prop to group by
+   * @param {String} sumProp - prop to summarize values for, if not provided 
+   *         this works as a count
+   */
+
+  /*const groupBy = (arr, prop, sumProp) => { // chnage this to return function that 
+    //groups per industy which can be fired upon an array. Later to be concatenated 
+    // with something that the prop that needs to be summed. Like revenue.
+    // select (count(x), sum(x.revenue)) from data group by x.industry 
+    let obj = {}
+    arr.forEach((x) => {
+      let v = x[prop]; 
+      let s = x[sumProp] || 1; 
+      v && obj[v] ? obj[v] = obj[v] + s : obj[v] = s;
+    });
+    return obj;
+  }*/
+
+  const groupBy = (arr, prop, sumProp) => { // chnage this to return function that 
+    //groups per industy which can be fired upon an array. Later to be concatenated 
+    // with something that the prop that needs to be summed. Like revenue.
+    // select (count(x), sum(x.revenue)) from data group by x.industry 
+    let obj = {}
+    arr.forEach((x) => {
+      let v = x[prop];
+      let s = x[sumProp] || 1;
+      v && obj[v] ? obj[v] = obj[v] + s : obj[v] = s;
+    });
+    return obj;
+  }
+
+  const toHighmapsFormat = (obj) => {
+    let arr = []
+    Object.entries(obj).forEach(x => {
+      let o = {}
+      o['hc-key'] = x[0];
+      o.value = x[1];
+      arr.push(o);
+    });
+    return arr;
+  }
+
+  /**
+   * 
+   * @param {*} data 
+   */
+  function Datasource(data) {
+    let self = this;
+    let model = {};
+    let bindings = [];
+    let charts = []; 
+    let filters = [];
+    let value; 
+    let baseData = data;
+  
+
+    // charts.push({chart: chart, filters: [filter1, filter2]}) // Highhcarts 
+    // setdata with filters and apply to chart, based upon baseData
+
+    this.setter = function (val) {
+      // update bindings; TODO: is it setting also the element that triggert the update?
+      bindings.forEach((b) => { 
+        // if supplied run callback on val parameter
+        let v = b.callback ? b.callback(val) : val;
+        console.log('update element with ' + v);
+        b.elem[b.prop] = v;
+      });
+
+      charts.forEach((c) =>{
+        console.log('update chart', c.chart);
+      })
+      value = val;
+    }
+  
+    this.getter = function () {
+      return value;
+    }
+
+    /**
+     * Apply all filters on the base-data
+     */
+    this.applyFilters = function() {
+      value = update(filters, baseData);
+    }
+  
+    Object.defineProperty(model, 'data', {
+      get: this.getter,
+      set: this.setter
+    })
+  
+    // TODO: if prop isn't set then apply callback
+    model.addBinding = function (element, prop, eventType, callback) {
+      element.addEventListener(eventType, function () {
+        console.log(element[prop])
+        self.setter(element[prop])
+      })
+      bindings.push({ elem: element, prop: prop, callback: callback });
+      element[prop] = value;
+      return this;
+    }
+
+    model.addFilter = function(filter) {
+      filters.push(filter);
+      self.applyFilters(); 
+    }
+
+    model.bindChart = function (chart, callback) {
+      charts.push({ chart: chart, cb: callback });
+    }
+  
+    model['data'] = data;
+    return model;
+  }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  //var obj = {data: 123}
+  //var input1 = document.getElementById("myText1")
+  //var input2 = document.getElementById("myText2")
+  //var myDOMElement = document.getElementById("myDomElement")
+
+  //let t = new binding(obj)
+  //t.addBinding(myInputElement1, "value", "keyup")
+  //.addBinding(myInputElement2, "value", "keyup")
+  //.addBinding(myDOMElement, "innerHTML")
+  //obj.a = 456;
+
+
+  document.querySelectorAll('input[type=radio]').forEach(
+    (x) => x.addEventListener('click', (e) => console.log(e))
+    );
+
+
+  const setupDataViz = (data) => {
+    console.log('hi')
+    console.log(data.length, data);
+    console.log(data[2]);
+
+    let ds = new Datasource(data);
+
+    let stateIsIl = matches('state_s', 'IL');
+    let stateIsWI = matches('state_s', 'WI');
+    let wiOrIl = or(stateIsIl,stateIsWI);
+    
+    
+    
+    
+    //console.log(ds.data);
+
+    //let groupByState = groupBy(ds.data, 'state_s', 'revenue');
+    let groupByState = groupBy(ds.data, 'state_s');
+    let groupByIndustry = groupBy(ds.data, 'industry');
+    
+
+    let mapDataByState = toHighmapsFormat(groupByState)
+    
+    console.log(mapDataByState);
+    //let groupByIndustry = groupBy(data,'industry', 'revenue');
+    console.log(groupByIndustry);
+    // built industry predicate first
+    
+    let softwarePred = matches('industry', 'Software');
+    let statePred = matches('state_l', 'Wisconsin');
+    let softwareInWisconsin = and(softwarePred, statePred);
+
+    console.log(data.filter(softwareInWisconsin));
+
+    // column chart
+    let categories = Highcharts.chart('column', {
+      title: {
+        text: 'Industries'
+      },
+      xAxis: {
+        categories: Object.keys(groupByIndustry)
+      },
+      series: [{
+        type: 'column',
+        data: Object.values(groupByIndustry)
+      }]
+    })
+    // map
+    let map = Highcharts.mapChart("map", {
+
+      chart: {
+        map: 'countries/us/us-all',
+        borderWidth: 1
+      },
+      legend: {
+        layout: "vertical",
+        align: "right",
+        verticalAlign: "middle"
+      },
+
+      colorAxis: {
+        min: 0,
+        minColor: "#E6E7E8",
+        maxColor: "#005645"
+      },
+
+      mapNavigation: {
+        enabled: true,
+        buttonOptions: {
+          verticalAlign: "bottom"
+        }
+      },
+
+      plotOptions: {
+        map: {
+          states: {
+            hover: {
+              color: "#EEDD66"
+            }
+          },
+          events: {
+            click: function () {
+              console.log(arguments)
+            }
+          }
+        }
+      },
+
+      series: [
+        {
+          //data: [['us-wi',2]],
+          //data: [{code:'WI',value: 2}],
+          data: mapDataByState,
+          joinBy: ['postal-code', 'hc-key'], // should be otherway around
+          name: "USA",
+          dataLabels: {
+            enabled: true,
+            format: "{point.properties.postal-code}"
+          }
+        },
+        {
+          type: "mapline",
+          data: Highcharts.geojson(Highcharts.maps["countries/us/us-all"], "mapline"),
+          color: "silver",
+          enableMouseTracking: false,
+          animation: {
+            duration: 500
+          }
+        }
+      ],
+
+      drilldown: {
+        activeDataLabelStyle: {
+          color: "#FFFFFF",
+          textDecoration: "none",
+          textOutline: "1px #000000"
+        },
+        drillUpButton: {
+          relativeTo: "spacingBox",
+          position: {
+            x: 0,
+            y: 60
+          }
+        }
+      },
+      responsive: {
+        rules: [{
+          condition: {
+            maxWidth: 600
+          },
+          chartOptions: {
+            legend: {
+              align: 'center',
+              verticalAlign: 'bottom',
+              layout: 'horizontal'
+            }
+          }
+        }]
+      }
+    });
+
+    ds.bindChart(categories, () => console.log('boe'));
+    ds.bindChart(map, () => console.log('boe'));
+
+    // ds.addFilter(wiOrIl); BOTH WORK, add filter or direct setting ds.data
+    ds.data = update([wiOrIl], ds.data);
+
+  } // setupDataViz
+
+  /*var groupBy = function(xs, key) {
+    return xs.reduce(function(rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  };*/
+
+  asyncFetch('/js/inc5000-2018.json')
+    .then((d) => setupDataViz(d))
+    .catch((err) => console.log(err));
+
+}, false);
+
+
+var formInput = document.getElementById("inp");
+
+
+
+
+//let t1 = datasource([0,1,2])
+let t2 = new Datasource(10)
+
+//console.log('t1', t1.hello())
+//console.log('t2', t2.hello())
+
+//t1.data = [0,1,3];
+t2.data = 1;
+let input1 = document.getElementById("myText1");
+let input2 = document.getElementById("myText2");
+let inputStart = document.getElementById("start");
+let inputCow = document.getElementById("cowbell");
+
+t2.addBinding(input1, 'value', 'keyup')
+  .addBinding(input2, 'value', 'keyup')
+  .addBinding(inputStart, 'value', "input")
+  .addBinding(inputCow, 'value', "input", function (val) {
+    return val / 5;
+  });
+
+
